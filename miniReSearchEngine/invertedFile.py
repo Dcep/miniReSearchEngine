42cb615372e9d4b5df1e930f865aad2b053b3e45
DCEP <mail@danielperic.com>
2015-03-18 15:02:43 +1100

    __init__.py 	css/ 	img/ 	index.html 	invertedFile.pyc 	js/

--- miniReSearchEngine/invertedFile.py
+++ miniReSearchEngine/invertedFile.py
@@ -1,41 +1,59 @@
 from collections import defaultdict
-import os, nltk
-
-l = lambda: defaultdict(l)
-
-invertedFile = l()
-
-for filename in os.listdir("lemma"):
-	with open("lemma/" + filename) as current_file:
-		tokens = nltk.word_tokenize(current_file.read())
-		counter = 0
-		for word in set(tokens):
-			try:
-				invertedFile[word]['DocCount'] += 1
-			except:
-				invertedFile[word]['DocCount'] = 1
-		for word in tokens:
-			counter += 1
-			try:
-				invertedFile[word]['FreqCount'] += 1
-			except:
-				invertedFile[word]['FreqCount'] = 1
-			try:
-				invertedFile[word]['docNo'][filename.split('.')[0]]['Freq'] += 1
-			except:
-				invertedFile[word]['docNo'][filename.split('.')[0]]['Freq'] = 1
-			try:
-				invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'].append(str(counter))
-			except:
-				invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'] = []
-				invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'].append(str(counter))
+import os, nltk, json
 
 
 
-for word in invertedFile:
-	print word
-	print 'DocCount:' + str(invertedFile[word]['DocCount'])
-	print 'FreqCount' + str(invertedFile[word]['FreqCount'])
-	for doc in invertedFile[word]['docNo']:
-		print 'docNo:' + str(doc) + ' Freq' +  str(invertedFile[word]['docNo'][doc]['Freq'])
-		print 'docNo:' + str(doc) + ' WordPosition' + str(invertedFile[word]['docNo'][doc]['WordPosition'])
+class miniReSearchIF():
+
+	def __init__(self):
+		filesDir = "lemma/"
+		#invertedFile = self.makeInvertedFile()
+
+
+	def l(self):
+		return defaultdict(self.l)
+
+	def makeInvertedFile(self):
+
+		invertedFile = self.l()
+
+		for filename in os.listdir(self.filesDir):
+			with open(self.filesDir + filename) as current_file:
+				tokens = nltk.word_tokenize(current_file.read())
+				counter = 0
+				for word in set(tokens):
+					try:
+						invertedFile[word]['DocCount'] += 1
+					except:
+						invertedFile[word]['DocCount'] = 1
+				for word in tokens:
+					counter += 1
+					try:
+						invertedFile[word]['FreqCount'] += 1
+					except:
+						invertedFile[word]['FreqCount'] = 1
+					try:
+						invertedFile[word]['docNo'][filename.split('.')[0]]['Freq'] += 1
+					except:
+						invertedFile[word]['docNo'][filename.split('.')[0]]['Freq'] = 1
+					try:
+						invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'].append(str(counter))
+					except:
+						invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'] = []
+						invertedFile[word]['docNo'][filename.split('.')[0]]['WordPosition'].append(str(counter))
+		return invertedFile
+
+
+
+
+if __name__ == '__main__':
+
+	mIF =  miniReSearchIF()
+	mIF.filesDir = "lemma/"
+	data = mIF.makeInvertedFile()
+	print(json.dumps(data['cleavage']))
+	print json.dumps(data['cleavage']['FreqCount'])
+	print json.dumps(data['cleavage']['DocCount'])
+	for i in data['cleavage']['docNo']:
+		print json.dumps(data['cleavage']['docNo'][i])
+
--- miniReSearchEngine/search.py
+++ miniReSearchEngine/search.py
@@ -1,1 +1,80 @@
+import sys, os
+
+from invertedFile import miniReSearchIF
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.web.server import Site
+from twisted.web.static import File
+
+import json
+
+from autobahn.twisted.websocket import WebSocketServerFactory, \
+	WebSocketServerProtocol
+
+from autobahn.twisted.resource import WebSocketResource, \
+	HTTPChannelHixie76Aware
+
+
+class EchoServerProtocol(WebSocketServerProtocol,miniReSearchIF):
+
+	def __init__(self):
+		miniIst = miniReSearchIF()
+		miniIst.filesDir = "lemma/"
+		self.invFile = miniIst.makeInvertedFile()
+		#miniReSearchIF.__init__()
+
+	def onConnect(self, request):
+		print("WebSocket connection request: {}".format(request))
+
+	def onMessage(self, payload, isBinary):
+		print "connected \n"
+		print payload
+		obj = json.loads(payload)
+		print obj['MSG']
+		if obj['TYPE'] == 'qry':
+			self.search(obj["MSG"])
+
+	def search(self, payload):
+		print payload
+		#for i in self.invFile[payload]:
+		data = json.dumps(self.invFile[payload], ensure_ascii=False)
+		data = json.loads(data)
+		print data
+		for i in data['docNo']:
+			with open("lemma/" + i + '.txt') as current_file:
+				payload = json.dumps({'id': i, "Job_Title": i, "Job_Requirements": current_file.read(), "Job_Description": "Nan"})
+				print payload
+				self.sendMessage(payload, isBinary=False)
+
+
+
+if __name__ == '__main__':
+
+	if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+		log.startLogging(sys.stdout)
+		debug = True
+	else:
+		debug = False
+
+	factory = WebSocketServerFactory("ws://localhost:8080",
+									debug=debug,
+									debugCodePaths=debug)
+
+	factory.protocol = EchoServerProtocol
+	factory.setProtocolOptions(allowHixie76=True)  # needed if Hixie76 is to be supported
+
+	resource = WebSocketResource(factory)
+
+	# # we server static files under "/" ..
+	root = File(".")
+
+	## and our WebSocket server under "/ws"
+	root.putChild("ws", resource)
+
+	## both under one Twisted Web Site
+	site = Site(root)
+	site.protocol = HTTPChannelHixie76Aware  # needed if Hixie76 is to be supported
+	reactor.listenTCP(8080, site)
+
+	reactor.run()
 __author__ = 'dan'
